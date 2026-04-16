7d21da6549f06700e6791d07a9194838100bd81b
hgibbs1219 <hgibbs@mcad.edu>
2026-04-16 11:32:32 -0500

    Add files via upload

---
+++ week12/exercise12/src/data/bookData.jsx
@@ -0,0 +1,45 @@
+export const books = [
+  {
+    id: 1,
+    title: "The Merchant of Venice",
+    author: "William Shakespeare",
+    content: "This is my content of Book 1",
+    bookCover: "./books/book1.jpg",
+  },
+  {
+    id: 2,
+    title: "The Picture of Dorian Gray",
+    author: "Oscar Wilde",
+    content: "This is my content of Book 2",
+    bookCover: "./books/book2.jpg",
+  },
+  {
+    id: 3,
+    title: "The Adventures of Sherlock Holmes",
+    author: "Arthur Conan Doyle",
+    content: "This is my content of Book 3",
+    bookCover: "./books/book3.jpg",
+  },
+  {
+    id: 4,
+    title: "Crime and Punishment",
+    author: "Fyodor Dostoevsky",
+    content: "This is my content of Book 4",
+    bookCover: "./books/book4.jpg",
+  },
+  {
+    id: 5,
+    title: "The Raven",
+    author: "Edgar Allan Poe",
+    content: "This is my content of Book 5",
+    bookCover: "./books/book5.jpg",
+  },
+  {
+    id: 6,
+    title: "CIA World Factbook",
+    author: "United States - Central Intelligence Agency",
+    content: "This is my content of Book 6",
+    bookCover: "./books/book6.jpg",
+  },
+
+];
---
+++ week12/exercise12/src/main.jsx
@@ -0,0 +1,8 @@
+import './css/index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
