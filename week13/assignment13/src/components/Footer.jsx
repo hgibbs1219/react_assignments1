d77145840976f7ea19f3693d5a473d82ff183a9b
hgibbs1219 <hgibbs@mcad.edu>
2026-04-26 20:42:13 -0500

    Delete week13/assignment13 directory

--- week13/assignment13/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <footer>
-            <p>&copy; 2026 Hannah Gibbs</p>
-            <p>This website was handcrafted with caffeine, questionable decisions, and at least one “why is this not working” moment.</p>
-        </footer>
-    );
-}
-export default Footer;
