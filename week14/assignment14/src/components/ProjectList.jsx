766024e111de9e882007ce8f505f9fd413009da5
hgibbs1219 <hgibbs@mcad.edu>
2026-05-01 11:30:02 -0500

    Add files via upload

---
+++ week14/assignment14/src/components/ProjectList.jsx
@@ -0,0 +1,19 @@
+import ProjectCard from "./ProjectCard";
+
+function ProjectList({ projects }) {
+  return (
+    <div className="project-list">
+      {projects.map((project) => (
+        <ProjectCard
+          key={project.id}
+          name={project.name}
+          image={project.image}
+          description={project.description}
+          url={project.url}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default ProjectList;
