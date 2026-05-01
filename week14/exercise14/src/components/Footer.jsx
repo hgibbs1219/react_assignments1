4ae34729ddc7d983f340e3211a2ed7bf19c9a785
hgibbs1219 <hgibbs@mcad.edu>
2026-05-01 11:31:54 -0500

    Delete week14 directory

--- week14/exercise14/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <footer>
-            <p>&copy; 2026 Hannah Gibbs</p>
-            <p>Stories live here, waiting for curious minds to open them.</p>
-        </footer>
-    );
-}
-export default Footer;
