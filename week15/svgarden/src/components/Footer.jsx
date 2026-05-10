5af794baa75dae4e06cdcd6fbc42ed56a558f8a3
hgibbs1219 <hgibbs@mcad.edu>
2026-05-10 11:53:26 -0500

    Add files via upload

---
+++ week15/svgarden/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+        <footer>
+            <p className="disclaimer">All SVGs are free for personal use only. Contact the site admin for commerical usage.</p>
+
+            <p>&copy; 2026 Hannah Gibbs</p>
+            <p>Warning: may cause spontaneous crafting.</p>
+
+        </footer>
+    );
+}
+export default Footer;
