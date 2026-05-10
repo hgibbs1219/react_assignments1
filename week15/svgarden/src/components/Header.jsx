b531fb372db1ed23f95deafe65386c63e05d015b
hgibbs1219 <hgibbs@mcad.edu>
2026-05-10 12:20:25 -0500

    Add files via upload

---
+++ week15/svgarden/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+    return (
+        <header className="site-header">
+
+            <div className="header-left">
+                <h1 className="logo">SVGarden</h1>
+                <p className="tagline">Where Creativity Takes Root</p>
+            </div>
+
+        </header>
+    );
+}
+
+export default Header;
